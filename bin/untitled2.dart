4baf1ec00b6084167d0fe3074569564d0d1b7772
EslamMourad648abd <63010762+EslamMourad648abd@users.noreply.github.com>
2021-11-18 11:22:22 +0200

    Initial commit

---
+++ bin/untitled2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  //The phrase that is displayed to the user once we run the program
+  print("Plese enter the number that you wish to create it's multiplication table :");
+  //Phrase to take the number from the user that he wishes to create it's multiplication table
+  num? num1=int.parse(stdin.readLineSync()!);
+  //For loop to run through number from 1 to 9
+  for(int i=0;i<=9;i++){
+    //printing ghe result of every number from 1 to 9 multiplied by the number that the user entered
+    print(i*num1!);
+  }
+}
